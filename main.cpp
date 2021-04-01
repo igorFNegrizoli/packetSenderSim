51f55b7ed49a1a2201d00fb90ee21c324a1e018b
igorFNegrizoli <igor.negrizoli@gmail.com>
2021-04-01 14:33:04 -0300

    Made main compatible with new testbench form

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include "testbench.hpp"
 #include <iostream>
 
+
 using namespace std;
 
 int main(){
 
+	cout << "Executando testes" << endl;
+	
 	testBench testes;
-	uint16_t count;
-	count = testes.doTest();
-	cout << count << endl;
+	uint16_t count, nTestes = 10;
+	for(uint16_t i=0; i<nTestes; ++i){
+		count = testes.doTest();
+		cout << count << ", ";	
+	}
+
+	cout << endl;
+	
+
+	/*
+	udpPacket pkg(8);
+	pkg.printPacket('b');
+	pkg.burstErrorPeriodicModel(8, 8, 2, 2);
+	pkg.printPacket('b');
+	cout << pkg.verifyChecksum() << endl;
+	*/
 
     return 0;
 }
