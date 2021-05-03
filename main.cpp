3472a0e88e8e668145a25a4b2c3f35ea3aa22f59
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-05-03 12:15:18 -0300

    test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-//#include "testbench.hpp"
+#include "CRC16Bit.hpp"
 #include "checksum16Bit.hpp"
 #include <iostream>
 
 int main(){
-
+/*
 	bool errorFlag;
 	uint16_t detectionFails = 0;
 
@@ -19,6 +19,20 @@ int main(){
 	}
 
 	std::cout << std::endl << detectionFails << std::endl;
+*/
 
+	checksum16Bit* pkg = new checksum16Bit(10);
+	pkg->printPacket('h');
+	CRC16Bit* pkg2 = new CRC16Bit(10);
+	std::cout << std::endl;
+	pkg2->printPacket('h');
+	
+	uint16_t* teste = pkg->getData();
+	for(int i=0; i<4; i++) std::cout << teste[i] << " " ;
+	std::cout << std::endl;
+	teste = pkg2->getData();
+	for(int i=0; i<4; i++) std::cout << teste[i] << " " ;
+	std::cout << std::endl;
+		
     return 0;
 }
