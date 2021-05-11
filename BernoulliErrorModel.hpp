88fe9dbfeba1221181b8d42130e4b9b85c7ea620
arluiz <55293550+arluiz@users.noreply.github.com>
2021-05-11 08:14:49 -0300

    Update BernoulliErrorModel.hpp

--- BernoulliErrorModel.hpp
+++ BernoulliErrorModel.hpp
@@ -20,25 +20,21 @@ class BernoulliErrorModel: public ErrorModel {
 	return this->BER;
      }
 
-    uint16_t injectErrors(Packet* packet) {
-	//packet->print('b');
+    uint16_t injectErrors(Packet* packet, bool forceError) {
     	uint16_t numberOfErrors = 0;
-    	//while(numberOfErrors==0){
-    	   //This loop's purpose is to ensure the packet has at least one error
-           for(uint16_t i = 0; i<packet->getLength()/2; ++i){
+    	for(uint16_t i = 0; i<packet->getLength()/2; ++i){
              for(uint16_t j = 15; j<16; --j){
                 if(getRNG()->trueFalseProb(this->getBER())==true){                    
                     numberOfErrors+=packet->injectErrorInChunk(i*16+j);
                 }
              }
-           }  
-	   if (numberOfErrors == 0)
-		 numberOfErrors+=packet->injectErrorInChunk(getRNG()->next(packet->getLength())); 
-    	//}
+        }  
+	if (numberOfErrors == 0 && forceError)
+	    numberOfErrors+=packet->injectErrorInChunk(getRNG()->next(packet->getLength())); 
+    	
         return numberOfErrors;
     }
 
 };
-
 
 
