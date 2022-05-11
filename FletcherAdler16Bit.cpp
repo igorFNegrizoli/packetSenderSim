df13ed58ed08f88641dc97bceebd207e0cbebc62
Igor França Negrizoli <igor.negrizoli@protonmail.com>
2022-05-11 13:34:24 -0300

    Added Fletcher and Adler algorithms

---
+++ FletcherAdler16Bit.cpp
@@ -0,0 +1,33 @@
+#include "FletcherAdler16Bit.hpp"
+//#include "Packet.hpp"
+#include <iostream>
+#include <bitset>
+
+/*
+(0,0,255) for ones complement fletcher
+(0,0,256) for twos complement fletcher
+(1,0,251) for adler
+*/
+FletcherAdler16Bit::FletcherAdler16Bit(uint16_t initialC1_, uint16_t initialC2_, uint16_t modulusVal_){
+    this->initialC1 = initialC1_;
+    this->initialC2 = initialC2_;
+    this->modulusVal = modulusVal_;
+}
+
+bool FletcherAdler16Bit::verifyChecksum(Packet* packet, uint16_t chk){
+    return(this->doChecksum(packet) == chk);
+}
+
+uint16_t FletcherAdler16Bit::doChecksum(Packet* packet){ 
+    uint8_t* data = (uint8_t*)packet->getData();
+    uint16_t c1 = this->initialC1;
+    uint16_t c2 = this->initialC2;
+
+    for(int i=0; i < packet->getLength(); i++){
+        c1 = (c1+data[i])%this->modulusVal;
+        c2 = (c2+c1)%this->modulusVal;
+        //std::cout << "c1: " << c1 << " - c2: " << c2 << std::endl;
+    }
+
+    return (uint16_t)(c1 << 8)+(uint8_t)c2;
+}
---
+++ FletcherAdler16Bit.hpp
@@ -0,0 +1,22 @@
+#ifndef FLETCHERADLER16BIT_HPP_INCLUDED
+#define FLETCHERADLER16BIT_HPP_INCLUDED
+#include <cstdint>
+#include "Packet.hpp"
+#include "VerificationAlgorithm.hpp"
+
+class FletcherAdler16Bit: public VerificationAlgorithm {
+private:
+    uint16_t initialC1;
+    uint16_t initialC2;
+    uint16_t modulusVal;
+
+public:
+    FletcherAdler16Bit(uint16_t initialC1_, uint16_t initialC2_, uint16_t modulusVal_);
+    bool verifyChecksum(Packet* packet, uint16_t chk);
+    uint16_t doChecksum(Packet* packet);
+    uint32_t generateVerificationCode(Packet* packet){
+        return (uint32_t) doChecksum(packet);
+    }
+};
+
+#endif
